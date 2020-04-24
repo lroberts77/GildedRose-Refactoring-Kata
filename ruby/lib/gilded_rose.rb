2222c396a8fd5464b1c80fbc3ab5929ac1f5f6db
liam roberts <liamroberts77@googlemail.com>
2020-04-24 21:19:40 +0100

    finishing touches, I will refactor further when i have time

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -38,6 +38,7 @@ class GildedRose
     end
   end
 end
+# THIS IS THE ORIGINAL CODE WHICH I HAVE REFACTORED ABOVE
 #       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
 #         if item.quality > 0
 #           if item.name != "Sulfuras, Hand of Ragnaros"
