3686c6b36f5191679c07a24db3249534ac4f4519
liam roberts <liamroberts77@googlemail.comgit config --global user.email liamroberts77@googlemail.com>
2020-04-25 13:49:22 +0100

    refactor v1

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -9,31 +9,31 @@ class GildedRose
 
       #Generic item
       if item.name != "Aged Brie" &&
-        item.name != "Backstage passes to a TAFKAL80ETC concert" &&
-        item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in > 0 ? item.quality = item.quality - 1 : item.quality = item.quality - 2 unless item.quality == 0
+         item.name != "Backstage passes to a TAFKAL80ETC concert" &&
+         item.name != "Sulfuras, Hand of Ragnaros"
+         item.sell_in > 0 ? item.quality -= 1 : item.quality -= 2 unless item.quality == 0
       end
       
       #Aged Brie
       if item.name == "Aged Brie"
-        item.sell_in > 0 ? item.quality = item.quality + 1 : item.quality = item.quality + 2 unless item.quality == 50
+         item.sell_in > 0 ? item.quality += 1 : item.quality += 2 unless item.quality == 50
       end
       
       # Backstage passes to a TAFKAL80ETC concert
       if item.name == "Backstage passes to a TAFKAL80ETC concert"
-        item.sell_in > 5 ? item.quality = item.quality + 2 : item.quality = item.quality + 3 unless item.quality == 50
+         item.sell_in > 5 ? item.quality += 2 : item.quality += 3 unless item.quality == 50
         if item.sell_in == 0 
-          item.quality = 0
+           item.quality = 0
         end
       end
 
       #Sulfuras, Hand of Ragnaros
       if item.name == "Sulfuras, Hand of Ragnaros"
-        item.quality = 80
-        item.sell_in = item.sell_in + 1
+         item.quality = 80
+         item.sell_in += 1
       end
 
-      item.sell_in = item.sell_in - 1
+      item.sell_in -= 1
 
     end
   end
