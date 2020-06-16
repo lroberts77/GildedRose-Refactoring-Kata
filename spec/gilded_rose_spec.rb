2bf2b3c45b65f4d4ea618ebfb28f152c604323a5
liam roberts <liamroberts77@googlemail.com>
2020-06-16 12:14:32 +0100

    added conjured items and tests2

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -40,7 +40,7 @@ describe GildedRose do
     end
 
     context "Conjured items" do
-      it "item quality decreses twice as fast as normal items" do
+      it "item quality decreases twice as fast as normal items" do
         items = [Item.new("Conjured", 10, 10)]
         GildedRose.new(items).update_quality()
         expect(items[0].quality).to eq (8)
